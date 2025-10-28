18be78fc8901f009a396cdedb2ad44a21829b277
Shiming Zhang <wzshiming@hotmail.com>
2025-10-28 14:15:14 +0800

    Fix DeleteAll and add DeleteBatch

--- sss_delete.go
+++ sss_delete.go
@@ -3,7 +3,6 @@ package sss
 import (
 	"context"
 	"errors"
-	"fmt"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -22,11 +21,11 @@ func (s *SSS) SignDelete(path string, expires time.Duration) (string, error) {
 		})
 }
 
+// Delete deletes the object stored at the given paths
 func (s *SSS) Delete(ctx context.Context, path string) error {
-	s3Path := s.s3Path(path)
 	_, err := s.s3.DeleteObjectWithContext(ctx, &s3.DeleteObjectInput{
 		Bucket: s.getBucket(),
-		Key:    aws.String(s3Path),
+		Key:    aws.String(s.s3Path(path)),
 	})
 	if err != nil {
 		return err
@@ -34,75 +33,71 @@ func (s *SSS) Delete(ctx context.Context, path string) error {
 	return nil
 }
 
-// DeleteAll recursively deletes all objects stored at "path" and its subpaths.
-// We must be careful since S3 does not guarantee read after delete consistency
-func (s *SSS) DeleteAll(ctx context.Context, path string) error {
-	s3Objects := make([]*s3.ObjectIdentifier, 0, listMax)
-	s3Path := s.s3Path(path)
-	listObjectsInput := &s3.ListObjectsV2Input{
-		Bucket: s.getBucket(),
-		Prefix: aws.String(s3Path),
-	}
-
-	for {
-		// list all the objects
-		resp, err := s.s3.ListObjectsV2WithContext(ctx, listObjectsInput)
-
-		// resp.Contents can only be empty on the first call
-		// if there were no more results to return after the first call, resp.IsTruncated would have been false
-		// and the loop would exit without recalling ListObjects
-		if err != nil || len(resp.Contents) == 0 {
-			return fmt.Errorf("path not found: %s", path)
+// DeleteBatch deletes multiple objects stored at the given paths
+func (s *SSS) DeleteBatch(ctx context.Context, paths []string) error {
+	var s3Objects []*s3.ObjectIdentifier
+	for i := 0; i < len(paths); i += listMax {
+		end := i + listMax
+		if end > len(paths) {
+			end = len(paths)
 		}
 
-		for _, key := range resp.Contents {
-			// Skip if we encounter a key that is not a subpath (so that deleting "/a" does not delete "/ab").
-			if len(*key.Key) > len(s3Path) && (*key.Key)[len(s3Path)] != '/' {
-				continue
-			}
+		for _, path := range paths[i:end] {
 			s3Objects = append(s3Objects, &s3.ObjectIdentifier{
-				Key: key.Key,
+				Key: aws.String(s.s3Path(path)),
 			})
 		}
 
-		// Delete objects only if the list is not empty, otherwise S3 API returns a cryptic error
-		if len(s3Objects) > 0 {
-			// NOTE: according to AWS docs https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListObjectsV2.html
-			// by default the response returns up to 1,000 key names. The response _might_ contain fewer keys but it will never contain more.
-			// 10000 keys is coincidentally (?) also the max number of keys that can be deleted in a single Delete operation, so we'll just smack
-			// Delete here straight away and reset the object slice when successful.
-			resp, err := s.s3.DeleteObjectsWithContext(ctx, &s3.DeleteObjectsInput{
-				Bucket: s.getBucket(),
-				Delete: &s3.Delete{
-					Objects: s3Objects,
-					Quiet:   aws.Bool(false),
-				},
-			})
-			if err != nil {
-				return err
-			}
-
-			if len(resp.Errors) > 0 {
-				// NOTE: AWS SDK s3.Error does not implement error interface which
-				// is pretty intensely sad, so we have to do away with this for now.
-				errs := make([]error, 0, len(resp.Errors))
-				for _, err := range resp.Errors {
-					errs = append(errs, errors.New(err.String()))
-				}
-				return errors.Join(errs...)
-			}
+		resp, err := s.s3.DeleteObjectsWithContext(ctx, &s3.DeleteObjectsInput{
+			Bucket: s.getBucket(),
+			Delete: &s3.Delete{
+				Objects: s3Objects,
+				Quiet:   aws.Bool(false),
+			},
+		})
+		if err != nil {
+			return err
 		}
-		// NOTE: we don't want to reallocate
-		// the slice so we simply "reset" it
+
 		s3Objects = s3Objects[:0]
 
-		// resp.Contents must have at least one element or we would have returned not found
-		listObjectsInput.StartAfter = resp.Contents[len(resp.Contents)-1].Key
+		if len(resp.Errors) > 0 {
+			errs := make([]error, 0, len(resp.Errors))
+			for _, err := range resp.Errors {
+				errs = append(errs, errors.New(err.String()))
+			}
+			return errors.Join(errs...)
+		}
+	}
+	return nil
+}
 
-		// from the s3 api docs, IsTruncated "specifies whether (true) or not (false) all of the results were returned"
-		// if everything has been returned, break
-		if resp.IsTruncated == nil || !*resp.IsTruncated {
-			break
+// DeleteAll recursively deletes all objects stored at path and its subpaths.
+// We must be careful since S3 does not guarantee read after delete consistency
+func (s *SSS) DeleteAll(ctx context.Context, path string) error {
+	var batch []string
+	err := s.Walk(ctx, path, func(fileInfo FileInfo) error {
+		if fileInfo.IsDir() {
+			return nil
+		}
+
+		batch = append(batch, fileInfo.Path())
+		if len(batch) == listMax {
+			if err := s.DeleteBatch(ctx, batch); err != nil {
+				return err
+			}
+			batch = batch[:0]
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if len(batch) > 0 {
+		if err := s.DeleteBatch(ctx, batch); err != nil {
+			return err
 		}
 	}
 
